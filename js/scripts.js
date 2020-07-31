aded90923f7d9e001e4ef4a4df1aefbe9c464f12
David Okwacha <osweredavid@gmail.com>
2020-08-01 00:17:36 +0300

    made a constructor and prototype in the script file

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ $(document).ready(function(){
         $("#make-order").toggle("5000")
     });
 });
+
+function Results (type,size,crust,toppings) {
+    this.type = type;
+    this.size= size;
+    this.crust= crust;
+    this.toppings=toppings;
+};
+Results.prototype.order = function() {
+    return "You have made an order of " + this.type + " pizza  with " + this.toppings + " as toppings and " + this.crust + " for crust ."
+};

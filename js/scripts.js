0350b4b7e5b06bfd776d5b7eee7b711e3387320b
David Okwacha <osweredavid@gmail.com>
2020-08-01 00:49:51 +0300

    made arrays to accommodate puzza names and prices

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,10 @@ function TotalPrice (price, quantity, delivery,toppings,crust) {
 TotalPrice.prototype.finalTotal = function () {
     return ( this.price + this.delivery + this.toppings + this.crust )* this.quantity ;
 };
+
+var sizePrices = [1200, 800, 600]
+var priceToppings = [100,120,80,150,200]
+var toppingsName= ["Pepperoni" , "mushroom" , "onion" ,"sausage", " bacon"]
+var crustNames= ["Crispy", "Stuffed", "Glutton-free"]
+var crustPrices = [100,120,200]
+var deliveryPrices = [0, 200];
